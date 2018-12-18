ac303896c4657f849d4ec2817a939ca9db727118
Cesar Omar Sanchez <30625358+cesaromar1973@users.noreply.github.com>
2018-12-18 09:18:12 -0500

    Annoying Prompt Message Window
    A prompt message will continue to ask "Are We There Yet?", if you answer, it will continue on, if you answer "YES" it will stop.

---
+++ annoy.js
@@ -0,0 +1,14 @@
+var answer = prompt("ARE WE THERE YET?");
+
+while(answer !== "yes" && answer !== "yeah"){
+	var answer = prompt("ARE WE THERE YET?");
+}
+
+alert("YAY, WE MADE IT!")
+
+// if(answer === "yes") {
+// 	alert("YAY, WE MADE IT!")
+// } else {
+// 	var answer = prompt("ARE WE THERE YET?")
+// }
+
